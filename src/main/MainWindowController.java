d0adba37843790d1de8d2ef05e0067c813a58efd
wuYou <wuyoutech@gmail.com>
2018-10-17 00:06:49 +0800

    添加Controller的Initializable接口
    给菜单-退出添加退出事件

--- src/main/MainWindowController.java
+++ src/main/MainWindowController.java
@@ -1,9 +1,18 @@
 package main;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class MainWindowController {
-    @FXML private Button mainButton;
+public class MainWindowController implements Initializable {
+    @FXML private MenuItem exitMenuItem;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        exitMenuItem.setOnAction(t->System.exit(0));
+    }
 }
