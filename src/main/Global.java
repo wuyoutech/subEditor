33a03b25ecbdec0e1f162863b56e140faa987438
wuYou <wuyoutech@gmail.com>
2018-10-16 23:49:51 +0800

    初始化

---
+++ src/main/Global.java
@@ -0,0 +1,5 @@
+package main;
+
+public class Global {
+    public boolean isChanged = false;
+}
---
+++ src/main/MainWindowController.java
@@ -0,0 +1,9 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class MainWindowController {
+    @FXML private Button mainButton;
+}
