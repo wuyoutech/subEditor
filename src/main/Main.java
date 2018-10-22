2da970fe7c5411c1748c46099cb27ddbbb2180d5
wuyoutech <wuyoutech@gmail.com>
2018-10-22 14:58:51 +0800

    加入样式载入至内存

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main extends Application {
     public static Stage main;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println(MessageFormat.format("{0}:{1} has exception:{2}",
+                        new SimpleDateFormat(("yyyy-MM-dd HH:mm:ss")).format(new Date()),t.getName(),e.toString()));
+            }
+        });
+
+
         main = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
         primaryStage.setTitle("subEditor");
--- src/main/MainWindowController.java
+++ src/main/MainWindowController.java
@@ -3,23 +3,21 @@ package main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
+import utils.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class MainWindowController implements Initializable {
-    @FXML public BorderPane mainBorderPane;
-
     @FXML public MenuItem exitMenuItem;
     @FXML private MenuItem newMenuItem;
     @FXML private MenuItem openMenuItem;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        newMenuItem.setOnAction(t -> Global.New());
+        //newMenuItem.setOnAction(t ->GlobalInfo);
         exitMenuItem.setOnAction(t -> System.exit(0));
-        openMenuItem.setOnAction(t->Global.Open());
+        openMenuItem.setOnAction(t->GlobalInfo.open());
     }
 }
